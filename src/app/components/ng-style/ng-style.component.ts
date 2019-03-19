4a2f0b4c5b3c657ed56e6dfe9afc929a97c61382
Angel <mcinegas@gmail.com>
2019-03-19 19:52:08 +0100

    btn plus-minus ok

--- src/app/components/ng-style/ng-style.component.ts
+++ src/app/components/ng-style/ng-style.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-style',
   template: `
-    <p>
-      ng-style works!
-    </p>
+  <p [style.fontSize.px] = "size">
+    Hola! Esto es una prueba...
+  </p>
+
+  <button class="btn btn-primary" (click)="size = size - 5">
+    <i class="fa fa-minus"></i>
+  </button>
+  <button class="btn btn-primary ml-2" (click)="size = size + 5">
+    <i class="fa fa-plus"></i>
+  </button>
+  
   `,
   styles: []
 })
 export class NgStyleComponent implements OnInit {
+
+  size:number = 20;
 
   constructor() { }
 
