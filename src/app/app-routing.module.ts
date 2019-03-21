b5edff8e8abf7460fce026ff4f37ae4fb072d921
Angel <mcinegas@gmail.com>
2019-03-21 16:37:54 +0100

    ruta anyiadida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
+  { path: 'user', component: UserComponent},
   { path: '**', redirectTo: 'home'}
 ];
 
