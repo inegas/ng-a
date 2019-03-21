eb5c0d3ce027dca624f98b4ad65d38c1a103f18c
Angel <mcinegas@gmail.com>
2019-03-21 16:17:26 +0100

    nuevo c home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { CssComponent } from './components/css/css.component';
 import { ClasesComponent } from './components/clases/clases.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { ClasesComponent } from './components/clases/clases.component';
     NgStyleComponent,
     CssComponent,
     ClasesComponent,
+    HomeComponent,
     
   ],
   imports: [
