9bc71972d75f7618846622cba068e51901b90a04
Angel <mcinegas@gmail.com>
2019-03-21 18:17:28 +0100

    sub rutas user ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
+import { NewUserComponent } from './components/user/new-user.component';
+import { EditUserComponent } from './components/user/edit-user.component';
+import { DetailtUserComponent } from './components/user/detailt-user.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'user', component: UserComponent},
+  { 
+    path: 'user/:id', 
+    component: UserComponent,
+    children: 
+    [
+      { path: 'new', component: NewUserComponent},
+      { path: 'edit', component: EditUserComponent},
+      { path: 'detail', component: DetailtUserComponent},
+    ]
+  },
   { path: '**', redirectTo: 'home'}
 ];
 
