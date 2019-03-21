e031dcd848477232ae132dbd74b9173b99720843
Angel <mcinegas@gmail.com>
2019-03-21 18:25:37 +0100

    subruta user-edit por defecto, ya que por default paso un id, tiene mas sentido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
       { path: 'new', component: NewUserComponent},
       { path: 'edit', component: EditUserComponent},
       { path: 'detail', component: DetailtUserComponent},
-      { path: '**', redirectTo: 'new'}
+      { path: '**', redirectTo: 'editar'}
     ]
   },
   { path: '**', redirectTo: 'home'}
